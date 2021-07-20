0fc67a273dd10f321b93846aa33e6f69c0320384
RadkaValkova <76747070+RadkaValkova@users.noreply.github.com>
2021-07-20 19:20:09 +0300

    Create views.py

---
+++ petstagram/accounts/views.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import logout, login
+from django.shortcuts import render, redirect
+
+from petstagram.accounts.forms import LoginForm, RegisterForm
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('landing page')
+    else:
+        form = LoginForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('landing page')
+    else:
+        form = RegisterForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('landing page')
